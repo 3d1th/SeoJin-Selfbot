cde9c8b5c7b2b21b1648032900e557742607b467
3d1th <atrixpyro1@gmail.com>
2024-09-16 07:17:27 +0900

    Refactor code structure and add new features to Toast class

--- func/toast.js
+++ func/toast.js
@@ -1,20 +1,24 @@
 import notifier from 'node-notifier';
+import path from 'path'; 
 
 class Toast {
     constructor() {
         this.defaultTitle = 'Notification';
         this.defaultMessage = 'You have a new notification!';
+        this.iconPath = path.join(__dirname, '../pics/icon.png'); 
+        this.appName = 'Seojin selfbot'; 
     }
 
     show(title, message) {
         notifier.notify({
             title: title || this.defaultTitle,
             message: message || this.defaultMessage,
+            icon: this.iconPath, 
             sound: true,
-            wait: false
+            wait: false,
+            appName: this.appName 
         });
     }
 }
-
 
 export default Toast;
